14add0c27901d4bb8e5777e4bcceb9ef6cc425f1
Radek <radoslawef@gmail.com>
2022-10-22 15:52:24 +0200

    js work in progress

--- js/main.js
+++ js/main.js
@@ -1,35 +1,30 @@
- const navMobile = document.querySelector ('.nav-mobile');
- const navBtn = document.querySelector ('.hamburger');
- const footerYear = document.querySelector('.footer__year')
- const allNavItems = document.querySelectorAll('.nav__link')
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const allNavItems = document.querySelectorAll('.nav__link')
+const bodyScroll = document.querySelector('body')
 
- const handleNav = () => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
 
-    allNavItems.forEach( item => {
-      item.addEventListener ('click', () => {
-         navMobile.classList.remove ('nav-mobile--active'),
-         navBtn.classList.remove('is-active')
-      })
-    })
- }
- navBtn.addEventListener('click', handleNav)
-
-
-
-
-
-
-
-
-
-
-
-
-
- const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active'), navBtn.classList.remove('is-active')
+		})
+	})
 }
-handleCurrentYear ();
+// no scroll temporary
+   const noScroll = () => {
+      navMobile.classList.remove('nav-mobile--active'), navBtn.classList.remove('is-active')
+   }
+
+
+navBtn.addEventListener('click', handleNav)
+bodyScroll.addEventListener('scroll', noScroll)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year;
+}
+handleCurrentYear();
